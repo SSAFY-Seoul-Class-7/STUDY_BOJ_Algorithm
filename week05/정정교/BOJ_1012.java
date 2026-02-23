1eb0f57b5151dd452c4a041dad446fb035f32b87
Willy <willy_1020@naver.com>
2026-02-23 22:53:06 +0900

    [DOCS] Week5 알고리즘 과제 제출

---
+++ week05/정정교/BOJ_1012.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class BOJ_1012 {
+
+	static StringTokenizer st;
+	static int[][] map;
+	static int N, M, K;
+	static boolean[][] visited;
+	static int answer;
+	static final int[] dr = {-1, 1, 0, 0};
+	static final int[] dc = {0, 0, -1, 1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 0; t < T; t++) {
+			answer = 0;
+			st = new StringTokenizer(br.readLine());
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+
+			//초기화
+			map = new int[N][M];
+			visited = new boolean[N][M];
+			for(int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				int b = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				map[a][b] = 1; //배추 표시
+			}
+
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < M; j++) {
+					if(!visited[i][j] && map[i][j] == 1) {
+						++answer;
+						dfs(i, j);
+					}
+				}
+			}
+
+			System.out.println(answer);
+		}
+	}
+
+	private static void dfs(int r, int c) {
+		visited[r][c] = true;
+
+		for(int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+
+			if(nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+			if(!visited[nr][nc] && map[nr][nc] == 1) {
+				dfs(nr, nc);
+			}
+		}
+	}
+}
---
+++ week05/정정교/BOJ_2667.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class BOJ_2667 {
+
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static StringTokenizer st;
+	static final int[] dr = {-1, 1, 0, 0};
+	static final int[] dc = {0, 0, -1, 1};
+	static int groupCnt, cnt;
+	static List<Integer> answer;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		answer = new ArrayList<>();
+		visited = new boolean[N][N];
+
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = str.charAt(j) - '0';
+			}
+		}
+
+		for(int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(!visited[i][j] && map[i][j] != 0) {
+					++groupCnt;
+					dfs(i, j);
+					answer.add(cnt);
+					cnt = 0;
+				}
+			}
+		}
+
+		System.out.println(groupCnt);
+		Collections.sort(answer);
+		for(int x : answer) {
+			System.out.println(x);
+		}
+	}
+
+	private static void dfs(int r, int c) {
+		visited[r][c] = true;
+		cnt++;
+
+		for(int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+
+			if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+			if(!visited[nr][nc] && map[nr][nc] == 1) {
+				dfs(nr, nc);
+			}
+		}
+	}
+
+}
