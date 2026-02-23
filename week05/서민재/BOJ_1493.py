0c3236f8d0ad8c6337ad3fbffdd84c6dd25d6503
seomj <smj100394@gmail.com>
2026-02-23 17:10:38 +0900

    [week05]서민재_1493

---
+++ week05/서민재/BOJ_1493.py
@@ -0,0 +1,27 @@
+# 박스 채우기
+# 블로그 참고 (https://velog.io/@ttc1018/%EB%B0%B1%EC%A4%80-1493%EB%B0%95%EC%8A%A4%EC%B1%84%EC%9A%B0%EA%B8%B0-Python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EA%B7%B8%EB%A6%AC%EB%94%94-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98)
+
+import sys
+input = sys.stdin.readline
+
+length, width, height = map(int, input().split())
+total = length * width * height
+N = int(input())
+cube = [tuple(map(int, input().split())) for _ in range(N)]
+cube.sort(reverse=True)
+
+answer, total_now = 0, 0
+for c_idx, c_cnt in cube:
+    total_now *= 8 # 다음 순서 = 이전 정육면체 부피의 1/8이므로 이전까지의 개수에 8을 곱해줌 (예 : 4*4*4 1개 = 2*2*2 8개)
+    c_len = 2**c_idx
+    
+    cnt_limit = (length // c_len) * (width // c_len) * (height // c_len) - total_now # 현재 공간에 채울 수 있는 개수 - 지금까지 채운 개수
+    cnt_limit = min(c_cnt, cnt_limit) # 실제로 채우기 가능한 개수
+    
+    answer += cnt_limit
+    total_now += cnt_limit
+
+if total_now == total:
+    print(answer)
+else:
+    print(-1)
